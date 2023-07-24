e62fc6457a4567eaee21fb656463bd57500a4904
elio1fiore <elio1.fiore@gmail.com>
2023-07-24 20:41:11 +0200

    implementation one

--- lib/feature/one_input/presentation/one_result_view.dart
+++ lib/feature/one_input/presentation/one_result_view.dart
@@ -15,134 +15,125 @@ class OneResultView extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final noty = ref.read(oneInputNotifierProvider.notifier);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Result'),
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Card(
-                  borderOnForeground: false,
-                  color: Colors.green.shade100,
-                  shape: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(
-                      color: Colors.green.shade700,
-                      width: 1.5,
-                    ),
-                  ),
-                  elevation: 2,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      children: [
-                        const Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            'Watt',
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        Text('${player.watt.toInt()}'),
-                      ],
-                    ),
-                  ),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Card(
+              borderOnForeground: false,
+              color: Colors.green.shade100,
+              shape: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(
+                  color: Colors.green.shade700,
+                  width: 1.5,
                 ),
-                const SizedBox(
-                  height: 15,
-                ),
-                ConstrainedBox(
-                  constraints: const BoxConstraints(
-                    maxHeight: 370,
-                  ),
-                  child: Card(
-                    elevation: 1.5,
-                    clipBehavior: Clip.antiAlias,
-                    shape: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(
-                        color: Colors.green.shade700,
-                        width: 1.5,
+              ),
+              elevation: 2,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Watt',
                       ),
                     ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.fromLTRB(20, 20, 0, 20),
-                          color: Colors.green.shade100,
-                          child: const Text('Time on meters'),
-                        ),
-                        Divider(
-                          color: Colors.green.shade700,
-                          height: 0,
-                        ),
-                        Flexible(
-                          child: ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: player.listTimeOnMeters.length,
-                            itemBuilder: (context, index) {
-                              final timeOnMeters =
-                                  player.listTimeOnMeters[index];
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Text('${player.watt.toInt()}'),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxHeight: 370,
+              ),
+              child: Card(
+                elevation: 1.5,
+                clipBehavior: Clip.antiAlias,
+                shape: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(
+                    color: Colors.green.shade700,
+                    width: 1.5,
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(20, 20, 0, 20),
+                      color: Colors.green.shade100,
+                      child: const Text('Time on meters'),
+                    ),
+                    Divider(
+                      color: Colors.green.shade700,
+                      height: 0,
+                    ),
+                    Flexible(
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: player.listTimeOnMeters.length,
+                        itemBuilder: (context, index) {
+                          final timeOnMeters = player.listTimeOnMeters[index];
 
-                              return Container(
-                                height: 50,
-                                decoration: BoxDecoration(
-                                  border: Border(
-                                    bottom: BorderSide(
-                                        color: Colors.green.shade700, width: 1),
-                                  ),
-                                ),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  mainAxisAlignment: MainAxisAlignment.center,
+                          return Container(
+                            height: 50,
+                            decoration: BoxDecoration(
+                              border: Border(
+                                bottom: BorderSide(
+                                    color: Colors.green.shade700, width: 1),
+                              ),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
                                   children: [
-                                    Row(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.end,
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceAround,
-                                      children: [
-                                        // ignore: unnecessary_string_interpolations
-                                        Text('${timeOnMeters.metersAndUnit}'),
-                                        // ignore: unnecessary_string_interpolations
-                                        Text('${timeOnMeters.durationMask}'),
-                                      ],
-                                    ),
+                                    // ignore: unnecessary_string_interpolations
+                                    Text('${timeOnMeters.metersAndUnit}'),
+                                    // ignore: unnecessary_string_interpolations
+                                    Text('${timeOnMeters.durationMask}'),
                                   ],
                                 ),
-                              );
-                            },
-                          ),
-                        ),
-                      ],
+                              ],
+                            ),
+                          );
+                        },
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
-                  child: ElevatedButton(
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(vertical: 20),
-                      child: Text('Nuovo Calcolo'),
-                    ),
-                    onPressed: (() => noty.goAndResetInputPage()),
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: ElevatedButton(
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  child: Text('Nuovo Calcolo'),
+                ),
+                onPressed: (() => noty.goAndResetInputPage()),
+              ),
+            ),
+          ],
         ),
       ),
     );
